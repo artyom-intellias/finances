87431c18e247a9383376e182c916e08665d6886e
artyom-intellias <artem.dehtiar@intellias.com>
2022-10-14 19:30:30 +0300

    Add collapse

--- src/event_handlers.py
+++ src/event_handlers.py
@@ -7,18 +7,32 @@ def e_add_year_btn(e):
     el = js.document.getElementById("years_list")
     years_total = el.childElementCount + 1
 
-    new_li = create_el(type_='li', class_="year-list-item list-group-item sortable-item p-3")
+    li = create_el(type_='li', class_="year-list-item list-group-item sortable-item p-3 col-2")
+    el.append(li)
 
-    text = create_el(type_='p', class_="text-center", text=f"year {years_total}")
-    # text = js.document.createTextNode(f"year {years_total}")
-    new_li.appendChild(text)
-    el.append(new_li)
+    collapse_btn = create_el('button', class_="btn", custom={"type": "button",
+                                                             "data-bs-toggle": "collapse",
+                                                             "data-bs-target": f"#collapseExample_{years_total}",
+                                                             "aria-expanded": "false",
+                                                             "aria-controls": f"collapseExample_{years_total}",
+                                                             })
+    collapse_body = create_el(class_="collapse", id_=f"collapseExample_{years_total}")
 
-    interest_rate_input = create_el(type_='input', class_="interest_rate_input form-control bg-success text-white ")
-    new_li.append(interest_rate_input)
+    text1 = create_el(type_='p', class_="text-center", text=f"year {years_total}")
+    text2 = create_el(type_='p', class_="text-center", text=f"placeholder")
+    interest_rate_input = create_el(type_='input',
+                                    class_=f"interest_rate_input_{years_total} form-control bg-success text-white")
+    inflation_input = create_el(type_='input',
+                                class_=f"inflation_input_{years_total} form-control bg-danger text-white")
 
-    inflation_input = create_el(type_='input', class_="inflation_input form-control bg-danger text-white")
-    new_li.append(inflation_input)
+    li.appendChild(collapse_btn)
+    li.appendChild(collapse_body)
+
+    collapse_btn.appendChild(text1)
+    collapse_body.appendChild(text2)
+
+    collapse_body.appendChild(interest_rate_input)
+    collapse_body.appendChild(inflation_input)
 
 
 def e_remove_year_btn(e):
