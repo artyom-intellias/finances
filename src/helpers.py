0819ff02de290386f1eba0d8438a517e0f238732
artyom-intellias <artem.dehtiar@intellias.com>
2022-10-17 02:34:30 +0300

    Add error toggling

--- src/helpers.py
+++ src/helpers.py
@@ -1,5 +1,5 @@
 import js
-
+from pyodide.ffi import create_proxy
 
 def trigger_event(event):
     event(js.document.createEvent('Event'))
@@ -23,20 +23,25 @@ def create_el(type_='div', id_=None, class_=None, text='', **kwargs):
     return el
 
 
-def validate_input(el, max, min=0):
+def validate_input(el, max, min=0, error_timeout=1000):
     val = el.value
+    class_toggle = create_proxy(lambda: el.classList.remove('is-invalid'))
     if val.isdigit():
         if int(val) > max:
             el.classList.add('is-invalid')
+            js.window.setTimeout(class_toggle, error_timeout)
             return ''
         elif int(val) < min:
             el.classList.add('is-invalid')
+            js.window.setTimeout(class_toggle, error_timeout)
             return ''
         else:
             el.classList.remove("is-invalid")
+            js.window.setTimeout(class_toggle, error_timeout)
             return str(val)
     elif val == '':
         return ''
     else:
         el.classList.add('is-invalid')
+        js.window.setTimeout(class_toggle, error_timeout)
         return ''
