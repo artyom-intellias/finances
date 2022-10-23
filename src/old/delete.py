a59374d9a07065c55909c5755fa5c808ef5260d9
Artyom <degtyarartem1@gmail.com>
2022-10-23 05:28:35 +0300

    Refactor

--- src/old/delete.py
+++ src/old/delete.py
@@ -1,43 +1,44 @@
 
 devaluation = 1
-
-
 base_inf = 0.02
-acc = 0
-acc_non_invested = 0
-interest_rate = 2
-monthly_salary = 3000
+interest_rate = 5
+initial = 0
+initial_non_invested = 0
 
-for i in range(20):
+monthly_salary = 3000
+wanted_monthly_income = 3000
+
+expected_lifespan = 60
+
+
+for i in range(30):
     print(f' year {i}')
 
     devaluation *= 1 - base_inf
     print(f'{devaluation=:.2f}')
-    acc += monthly_salary * 12
+    initial += monthly_salary * 12
 
-    before_divs = acc
-    acc_non_invested += monthly_salary * 12
-    acc *= 1 + interest_rate * 0.01
+    before_profit = initial
+    initial_non_invested += monthly_salary * 12
+    initial *= 1 + interest_rate * 0.01
 
-    pure_profit = acc - before_divs
-    pure_profit_monthly = pure_profit /12
+    pure_profit = initial - before_profit
+    pure_profit_monthly = pure_profit / 12
     pure_real_profit = pure_profit * devaluation
     pure_real_profit_monthly = pure_real_profit / 12
-    pure_acc_non_invested = acc_non_invested * devaluation
-    print(f"{acc_non_invested=}")
-    print(f"{acc=}")
-    print(f"{pure_profit=}")
-    print(f"{pure_real_profit=}")
-    print(f"{pure_acc_non_invested=}")
-    print(f"{acc=}")
-    print(f"{pure_profit_monthly=}")
-    print(f"{pure_real_profit_monthly=}")
+    pure_initial_non_invested = initial_non_invested * devaluation
+
+    inflated_yearly = base_inf * initial
+    inflated_monthly = inflated_yearly / 12
+
+    print(f"{initial=:.0f} {initial_non_invested=:.0f} {pure_initial_non_invested=:.0f}")
+    print(f"{pure_profit=:.0f} {pure_real_profit=:.0f} {pure_profit_monthly=:.0f} {pure_real_profit_monthly=:.0f}")
+    print(f"{inflated_yearly=:.0f} {inflated_monthly=:.0f}")
     print()
 
-real_profit = acc * devaluation
-acc_non_invested_and_inflated = acc_non_invested * devaluation
-
-
-print(f'{devaluation}')
-print(f'{acc=:.2f} {real_profit=:.2f}')
-print(f'{acc_non_invested=:.2f} {acc_non_invested_and_inflated=:.2f}')
+    if pure_real_profit_monthly > wanted_monthly_income:
+        print('wow')
+        # break
+    if inflated_monthly > wanted_monthly_income:
+        print('fucking hell!')
+        break
