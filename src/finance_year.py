002ede80601bf4df4786e4f72955e9cff22a3bdf
Artyom <degtyarartem1@gmail.com>
2022-10-25 03:22:55 +0300

    Add balance to finance_year.py

--- src/finance_year.py
+++ src/finance_year.py
@@ -29,18 +29,25 @@ class FinanceYear:
     def generate_report(self):
         self.initial += self.monthly_salary * 12
         devaluation_rate = self.devaluation_rate * (1 - (self.inflation_rate * Decimal(0.01)))
-        yearly_salary = self.monthly_salary * 12
+
         yearly_expenses = self.monthly_expenses * 12
+        yearly_salary = self.monthly_salary * 12
+
         monthly_salary_indexed = self.monthly_salary + (self.monthly_salary * self.inflation_rate * Decimal(0.01))
         yearly_salary_indexed = monthly_salary_indexed * 12
         monthly_expenses_indexed = self.monthly_expenses + (self.monthly_expenses * self.inflation_rate * Decimal(0.01))
         yearly_expenses_indexed = monthly_expenses_indexed * 12
-        yearly_income = (self.initial + yearly_salary) * (self.interest_rate * Decimal(0.01) + 1)
+
+
+        yearly_income = (self.initial + yearly_salary - yearly_expenses) * (self.interest_rate * Decimal(0.01) + 1)
         monthly_income = yearly_income / 12
+
         yearly_adjusted_income = yearly_income * self.devaluation_rate
         monthly_adjusted_income = yearly_adjusted_income / 12
+
         yearly_inflated = (yearly_salary + yearly_income) * (1 - (self.inflation_rate * Decimal(0.01)))
         monthly_inflated = yearly_inflated / 12
+
         total_income = self.previous_income + yearly_income
         total_adjusted_income = total_income * self.devaluation_rate
         total_inflated = self.previous_inflation + yearly_inflated
@@ -49,20 +56,27 @@ class FinanceYear:
             "interest_rate": self.interest_rate,
             "inflation_rate": self.inflation_rate,
             "devaluation_rate": devaluation_rate,
+
             "monthly_salary": monthly_salary_indexed if self.is_index_salary else self.monthly_salary,
             "monthly_expenses": monthly_expenses_indexed if self.is_index_expenses else self.monthly_expenses,
+
             "yearly_salary": yearly_salary,
             "yearly_expenses": yearly_expenses,
+
             "monthly_salary_indexed": monthly_salary_indexed,
             "yearly_salary_indexed": yearly_salary_indexed,
             "monthly_expenses_indexed": monthly_expenses_indexed,
             "yearly_expenses_indexed": yearly_expenses_indexed,
+
             "yearly_income": yearly_income,
             "monthly_income": monthly_income,
+
             "yearly_adjusted_income": yearly_adjusted_income,
             "monthly_adjusted_income": monthly_adjusted_income,
+
             "yearly_inflated": yearly_inflated,
             "monthly_inflated": monthly_inflated,
+
             "total_income": total_income,
             "total_adjusted_income": total_adjusted_income,
             "total_inflated": total_inflated,
@@ -77,11 +91,20 @@ class FinanceYear:
 
 
 if __name__ == '__main__':
-    base_inf = 10
-    y = FinanceYear(inflation_rate=base_inf)
-    print(y.devaluation_rate)
-    acc = y.devaluation_rate
-    for i in range(4):
-        y = FinanceYear(inflation_rate=base_inf, devaluation_rate=acc)
-        acc = y.devaluation_rate
-        print(y.devaluation_rate)
+    inflation_rate = 5
+    interest_rate = 10
+    monthly_salary = 3000
+    monthly_expenses = 1000
+
+    y = FinanceYear(
+        inflation_rate=Decimal(inflation_rate),
+        interest_rate=Decimal(interest_rate),
+        monthly_salary=Decimal(monthly_salary),
+        monthly_expenses=Decimal(monthly_expenses),
+    )
+    from pprint import pprint
+
+    data = y.generate_report()
+
+    data_formatted = {key: round(int(value), 0) for key, value in data.items()}
+    pprint(data_formatted)
